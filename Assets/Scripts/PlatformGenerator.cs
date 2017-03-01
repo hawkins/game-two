317677056d7c70b6b6bb90936ce50c306d33240d
FlaxenTrousers <langolier009@gmail.com>
2017-03-01 16:31:17 -0600

    MadDash Unity Project
    Implemented infinitely spawning platforms and temporary character
    running animations.

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject platform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+    public ObjectPooler objectPool;
+
+	// Use this for initialization
+	void Start () {
+
+        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, 0);
+
+            //Instantiate(platform, transform.position, transform.rotation);
+            GameObject newPlatform = objectPool.GetPooledObject();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+        }
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D rbody;
+
+    public bool grounded;
+    public LayerMask isGround;
+
+    private Collider2D myCollider;
+
+    private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+
+        rbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+        myAnimator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        grounded = Physics2D.IsTouchingLayers(myCollider, isGround);
+
+        rbody.velocity = new Vector2(moveSpeed, rbody.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+
+            if(grounded) { rbody.velocity = new Vector2(rbody.velocity.x, jumpForce); }            
+        }
+
+        myAnimator.SetFloat("Speed", rbody.velocity.x);
+        myAnimator.SetBool("Grounded", grounded);
+	}
+}
