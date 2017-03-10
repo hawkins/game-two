bbca931f2173873ec3be00fb3d32128070f7ef87
FlaxenTrousers <langolier009@gmail.com>
2017-03-09 22:08:28 -0600

    New52
    Finalized version of everything. Includes screenshots and video as per
    required in the assignment description. Also includes installer for the
    game.

---
+++ Mad Dash/Assets/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Transform PlatformGenerator;
+    public Transform BackgroundGenerator;
+    private Vector3 backgroundStartPoint;
+    private Vector3 platformStartPoint;
+
+    public PlayerController thePlayer;
+    private Vector3 playerStartPoint;
+
+    private PlatformDestroyer[] stuffList;
+
+    private ScoreManager theScoreManager;
+
+    public DeathMenu theDeathScreen;
+
+	// Use this for initialization
+	void Start () {
+        platformStartPoint = PlatformGenerator.position;
+        backgroundStartPoint = BackgroundGenerator.position;
+        playerStartPoint = thePlayer.transform.position;
+
+        theScoreManager = FindObjectOfType<ScoreManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RestartGame()
+    {
+        theScoreManager.scoreIncreasing = false;
+        thePlayer.gameObject.SetActive(false);
+        theDeathScreen.gameObject.SetActive(true);
+
+        //StartCoroutine("RestartGameCo");
+    }
+
+    public void Reset()
+    {
+        theDeathScreen.gameObject.SetActive(false);
+        thePlayer.moveSpeed = thePlayer.moveSpeedStore;
+        thePlayer.speedMultiplier = thePlayer.speedMultiplierStore;
+        thePlayer.mushroomTracker = 0;
+        thePlayer.pillTracker = 0;
+        thePlayer.shroomCount.text = "x " + thePlayer.mushroomTracker;
+        thePlayer.pillCount.text = "x " + thePlayer.pillTracker;
+        stuffList = FindObjectsOfType<PlatformDestroyer>();
+        for (int i = 0; i < stuffList.Length; i++)
+        {
+            stuffList[i].gameObject.SetActive(false);
+        }
+
+        thePlayer.transform.position = playerStartPoint;
+        PlatformGenerator.position = platformStartPoint;
+        BackgroundGenerator.position = backgroundStartPoint;
+        thePlayer.gameObject.SetActive(true);
+
+        theScoreManager.scoreCount = 0;
+        theScoreManager.pointMultiplier = 1;
+        theScoreManager.scoreIncreasing = true;
+    }
+
+    /* public IEnumerator RestartGameCo()
+    {
+        theScoreManager.scoreIncreasing = false;
+        thePlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        stuffList = FindObjectsOfType<PlatformDestroyer>();
+        for(int i = 0; i < stuffList.Length; i++)
+        {
+            stuffList[i].gameObject.SetActive(false);
+        }
+
+        thePlayer.transform.position = playerStartPoint;
+        PlatformGenerator.position = platformStartPoint;
+        BackgroundGenerator.position = backgroundStartPoint;
+        thePlayer.gameObject.SetActive(true);
+
+        theScoreManager.scoreCount = 0;
+        theScoreManager.pointMultiplier = 1;
+        theScoreManager.scoreIncreasing = true;
+    } */
+}
---
+++ Mad Dash/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public Text scoreText;
+    public Text hiScoreText;
+
+    public float scoreCount;
+    public float trackScoreCount;
+    public float hiScoreCount;
+
+    public float pointsPerSecond;
+    public float pointMultiplier;
+
+    public bool scoreIncreasing;
+
+	// Use this for initialization
+	void Start () {
+		if(PlayerPrefs.HasKey("HighScore"))
+        {
+            hiScoreCount = PlayerPrefs.GetFloat("HighScore");
+        }
+        if (PlayerPrefs.HasKey("scoreCount"))
+        {
+            scoreCount = PlayerPrefs.GetFloat("scoreCount");
+        }
+        if (PlayerPrefs.HasKey("trackScoreCount"))
+        {
+            trackScoreCount = PlayerPrefs.GetFloat("trackScoreCount");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(scoreIncreasing)
+        {
+            scoreCount += pointsPerSecond * Time.deltaTime;
+            PlayerPrefs.SetFloat("scoreCount", scoreCount);
+        }
+
+        if(trackScoreCount > hiScoreCount)
+        {
+            hiScoreCount = trackScoreCount;
+            PlayerPrefs.SetFloat("HighScore", hiScoreCount);
+        }
+
+        trackScoreCount = scoreCount * pointMultiplier;
+        PlayerPrefs.SetFloat("trackScoreCount", trackScoreCount);
+        scoreText.text = "Score: " + Mathf.Round(scoreCount) + " x " + pointMultiplier;
+        hiScoreText.text = "High Score: " + (Mathf.Round(hiScoreCount));
+
+	}
+}
